30fe801d677ff7110a7538913c9e50357e939354
高中华 <gaozhonghua@gaozhonghuadeMacBook-Pro.local>
2017-09-21 21:55:32 +0800

    ingredient&direction but have a bug

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,7 +15,7 @@ class RecipesController < ApplicationController
   def create
     @recipe = Recipe.new(recipe_params)
     if @recipe.save
-      redirect_to recipes_path
+      redirect_to  @recipe,notice:"Successfully created new recipe"
     else
       render 'new'
     end
@@ -25,8 +25,9 @@ class RecipesController < ApplicationController
   end
 
   def update
+
     if @recipe.update(recipe_params)
-      redirect_to recipe_path
+      redirect_to @recipe
     else
       render 'edit'
     end
@@ -40,11 +41,14 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:title,:description,:image)
-  end
+    params.require(:recipe).permit(:title, :description, :image, ingredients_attributes: [:id, :name, :_destroy], directions_attributes: [:id, :step, :_destroy])
+	end
 
-  def find_recipe
-     @recipe = Recipe.find(params[:id])
-  end
+	def find_recipe
+		@recipe = Recipe.find(params[:id])
+	end
+
+
+
 
 end
